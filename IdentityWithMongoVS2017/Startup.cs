ce776de4ec642f11e4089d34dcc350b9eb7f4830
chilas <iamchilas@gmail.com>
2017-05-18 14:50:25 +0100

    fix: Resolve dependency on IConfigurationRoot

--- IdentityWithMongoVS2017/Startup.cs
+++ IdentityWithMongoVS2017/Startup.cs
@@ -45,6 +45,7 @@ namespace IdentityWithMongoVS2017
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
+            services.AddSingleton(provider => Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
