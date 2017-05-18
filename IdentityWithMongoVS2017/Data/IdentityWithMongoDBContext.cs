cabc3bd9b7976b5af7283cf917469f9ef1f9f798
chilas <iamchilas@gmail.com>
2017-05-18 10:39:43 +0100

    chore: create new IdentityWithMongoDBContext
    This will act as EFDbContext in standard EF implementation with SQL server.

---
+++ IdentityWithMongoVS2017/Data/IdentityWithMongoDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityWithMongoVS2017.Data
+{
+    public class IdentityWithMongoDBContext
+    {
+        private MongoClient _dbClient;
+        private string _connectionString;
+        private IConfigurationRoot _config;
+
+        public IdentityWithMongoDBContext(
+            IConfigurationRoot config
+            )
+        {
+            _config = config;
+            _connectionString = _config.GetConnectionString("DefaultConnection");
+            _dbClient = _dbClient ?? new MongoClient(_connectionString);
+        }
+    }
+}
