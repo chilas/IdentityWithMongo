2eac670c76a64922ede2f4b2a77d14998698b12c
chilas <iamchilas@gmail.com>
2017-05-18 10:46:17 +0100

    chore: create Person class

---
+++ IdentityWithMongoVS2017/Data/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityWithMongoVS2017.Data
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
